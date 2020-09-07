658f1cd128baeaca79b1e3dbd8e61d4376232e39
DanielDent11 <farctate11@gmail.com>
2020-09-07 20:19:38 +0300

    1) Added all 6 states
    2) Implemented cycle of states

--- Assets/TrafficLight/Scripts/TrafficLightController.cs
+++ Assets/TrafficLight/Scripts/TrafficLightController.cs
@@ -1,51 +1,188 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrafficLightController : MonoBehaviour
 {
+    public CycleItem[] cycle;
+    public Text currentStateText;
+
     private static TrafficLight trafficLight;
+    private IEnumerator cycleCoroutine;
+    private IEnumerator flashingCoroutine;
+
+    GameObject buttons;
 
     void Start()
     {
         trafficLight = new TrafficLight();
+        buttons = GameObject.Find("/Canvas/Buttons");
+        SetToOFF();        
+    }
+
+    void OnDisable()
+    {
+        SetToOFF();
+    }
+
+    public void FadeOut()
+    {
+        if (trafficLight.currentState == TrafficLight.States.OFF)
+        {
+            return;
+        }
+        else if (trafficLight.currentState == TrafficLight.States.Red)
+        {
+            StartCoroutine(FadeOutCoroutine(trafficLight.topLamp, 0.01f));
+        }
+        else if (trafficLight.currentState == TrafficLight.States.Yellow)
+        {
+            StartCoroutine(FadeOutCoroutine(trafficLight.middleLamp, 0.01f));
+
+        }
+        else if (trafficLight.currentState == TrafficLight.States.Green)
+        {
+            StartCoroutine(FadeOutCoroutine(trafficLight.bottomLamp, 0.01f));
+        }
+    }
+
+    public void SetToState(TrafficLight.States state)
+    {
+        if (state == TrafficLight.States.OFF)
+        {
+            SetToOFF();
+        }
+        else if (state == TrafficLight.States.Red)
+        {
+            SetToRed();
+        }
+        else if (state == TrafficLight.States.Yellow)
+        {
+            SetToYellow();
+        }
+        else if (state == TrafficLight.States.Green)
+        {
+            SetToGreen();
+        }
+        else if (state == TrafficLight.States.FlashingGreen)
+        {
+            SetToFlashingGreen();
+        }
+        else if (state == TrafficLight.States.FlashingRed)
+        {
+            SetToFlashingRed();
+        }
+        else if (state == TrafficLight.States.FlashingYellow)
+        {
+            SetToFlashingYellow();
+        }
+    }
+
+    public void RunCycle()
+    {
+        SetToOFF();
+        cycleCoroutine = CycleCoroutine();
+        StartCoroutine(cycleCoroutine);
+        foreach (Transform button in buttons.transform)
+        {
+            if (!button.gameObject.CompareTag("StopCycle"))
+            {
+                button.GetComponent<Button>().interactable = false;
+            }
+        }
+    }
+
+    public void StopCycle()
+    {
+        if (cycleCoroutine != null)
+        {
+            StopCoroutine(cycleCoroutine);
+            SetToOFF();
+        }
+        foreach (Transform button in buttons.transform)
+        {
+            button.GetComponent<Button>().interactable = true;
+        }
     }
 
     public void SetToRed()
     {
+        SetToOFF();
+        trafficLight.currentState = TrafficLight.States.Red;
+        currentStateText.text = "Red";
         trafficLight.topLamp.SetON();
         trafficLight.middleLamp.SetOFF();
         trafficLight.bottomLamp.SetOFF();
-        trafficLight.currentState = TrafficLight.States.Red;
     }
 
     public void SetToYellow()
     {
+        SetToOFF();
+        trafficLight.currentState = TrafficLight.States.Yellow;
+        currentStateText.text = "Yellow";
         trafficLight.topLamp.SetOFF();
         trafficLight.middleLamp.SetON();
         trafficLight.bottomLamp.SetOFF();
-        trafficLight.currentState = TrafficLight.States.Yellow;
     }
 
     public void SetToGreen()
     {
+        SetToOFF();
+        trafficLight.currentState = TrafficLight.States.Green;
+        currentStateText.text = "Green";
         trafficLight.topLamp.SetOFF();
         trafficLight.middleLamp.SetOFF();
         trafficLight.bottomLamp.SetON();
-        trafficLight.currentState = TrafficLight.States.Green;
     }
 
     public void SetToOFF()
     {
-        trafficLight.topLamp.SetOFF();
-        trafficLight.middleLamp.SetOFF();
-        trafficLight.bottomLamp.SetOFF();
+        if (flashingCoroutine != null)
+            StopCoroutine(flashingCoroutine);
         trafficLight.currentState = TrafficLight.States.OFF;
+        if (currentStateText != null)
+        {
+            currentStateText.text = "OFF";
+        }
+        if (trafficLight.topLamp != null) 
+            trafficLight.topLamp.SetOFF();
+        if (trafficLight.middleLamp != null)
+            trafficLight.middleLamp.SetOFF();
+        if (trafficLight.bottomLamp != null)
+            trafficLight.bottomLamp.SetOFF();
+    }
+
+    public void SetToFlashingRed(bool clearControllerStateBeforeSetting = true)
+    {
+        SetToOFF();
+        currentStateText.text = "FlashingRed";
+        trafficLight.currentState = TrafficLight.States.FlashingRed;
+        flashingCoroutine = FlashingCoroutine(trafficLight.topLamp, 0.2f);
+        StartCoroutine(flashingCoroutine);
+    }
+
+    public void SetToFlashingYellow(bool clearControllerStateBeforeSetting = true)
+    {
+        SetToOFF();
+        currentStateText.text = "FlashingYellow";
+        trafficLight.currentState = TrafficLight.States.FlashingYellow;
+        flashingCoroutine = FlashingCoroutine(trafficLight.middleLamp, 0.2f);
+        StartCoroutine(flashingCoroutine);
+    }
+
+    public void SetToFlashingGreen(bool clearControllerStateBeforeSetting = true)
+    {
+        SetToOFF();
+        currentStateText.text = "FlashingGreen";
+        trafficLight.currentState = TrafficLight.States.FlashingGreen;
+        flashingCoroutine = FlashingCoroutine(trafficLight.bottomLamp, 0.2f);
+        StartCoroutine(flashingCoroutine);
     }
 
     public class TrafficLight
     {
-        public enum States { OFF, Red, Yellow, Green, FlashingRed, FlashingGreen };
+        public enum States { OFF, Red, Yellow, Green, FlashingRed, FlashingGreen, FlashingYellow };
 
         public States currentState;
 
@@ -93,14 +230,74 @@ public class TrafficLightController : MonoBehaviour
 
         public void SetON()
         {
-            lampGameObject.GetComponent<MeshRenderer>().sharedMaterial.color = onColor;
+            lampGameObject.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_Color", onColor);
+            lampGameObject.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_EmissionColor", onColor);
+            lampGameObject.GetComponent<MeshRenderer>().sharedMaterial.EnableKeyword("_EMISSION");
             currentState = States.ON;
         }
 
         public void SetOFF()
         {
-            lampGameObject.GetComponent<MeshRenderer>().sharedMaterial.color = offColor;
+            lampGameObject.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_Color", offColor);
+            lampGameObject.GetComponent<MeshRenderer>().sharedMaterial.DisableKeyword("_EMISSION");
             currentState = States.OFF;
+        }
+
+        public void SetEmissionIntensity(float intensity)
+        {
+            var renderer = lampGameObject.GetComponent<MeshRenderer>();
+            Color oldColor = renderer.sharedMaterial.color;
+           // float factor = Mathf.Pow(2, intensity);
+            Color newColor = new Color((oldColor.r != 0f) ? 1f * intensity : 0f, (oldColor.g != 0f) ? 1f * intensity : 0f, (oldColor.b != 0f) ? 1f * intensity : 0f);
+            renderer.sharedMaterial.SetColor("_EmissionColor", newColor);
+            renderer.sharedMaterial.SetColor("_Color", newColor);
+            //DynamicGI.SetEmissive(renderer, newColor);
+        }
+    }
+
+    [System.Serializable]
+    public struct CycleItem
+    {
+        public TrafficLight.States state;
+        // Duration in seconds
+        public float duration;
+    }
+
+    private IEnumerator CycleCoroutine()
+    {
+        while (true)
+        {
+            for (int i = 0; i < cycle.Length; ++i)
+            {
+                SetToState(cycle[i].state);
+                yield return new WaitForSeconds(cycle[i].duration);
+            }
+        }
+    }
+
+    private IEnumerator FadeOutCoroutine(TrafficLightLamp lamp, float duration)
+    {
+        const float Step = 0.05f;
+        float intensity = 1f;
+        int numberOfFrames = Mathf.RoundToInt((intensity - 0.1f) / Step);
+        float delay = duration / numberOfFrames;
+        for (int i = 0; i < numberOfFrames; ++i)
+        {
+            lamp.SetEmissionIntensity(intensity);
+            intensity -= Step;
+            yield return new WaitForSeconds(delay);
+        }
+        lamp.SetOFF();
+    }
+
+    private IEnumerator FlashingCoroutine(TrafficLightLamp lamp, float sec)
+    {
+        while (true)
+        {
+            lamp.SetON();
+            StartCoroutine(FadeOutCoroutine(lamp, sec));
+            yield return new WaitForSeconds(sec + 0.4f);
         }
     }
 }
+
